f850ad0c05dbe4ff6d9f1f17d23162925c2d305d
alsap <98692361+alsap@users.noreply.github.com>
2022-04-01 15:55:32 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -6,7 +6,6 @@ float findMinValue(float numb1, float numb2, float numb3)
 		
 	float min_number;
 	min_number = min(min(numb1, numb2), numb3);
-	cout << "min number:" << min_number;
 	
 	return min_number;
 }
